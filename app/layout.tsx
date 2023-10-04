081054543d3e806bcc10d3a04a6f0d8ce1b96c9f
minjee <minjeeson.dev@gmail.com>
2023-10-04 15:18:06 +0100

    test: change user.name & user.email

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Minjee Son",
   description:
-    "I'm a dedicated Front End Developer with a passion for creating exceptional user experiences. I have mainly used React and TypeScript throughout my career and am highly proficient in the foundational frontend languages of HTML, CSS, and JavaScript. I'm also a lifelong learner who embraces new challenges.",
+    "I'm a dedicated Front End Developer with a passion for creating exceptional user experiences. I have mainly used React and TypeScript throughout my career and am highly proficient in the foundational frontend languages of HTML, CSS, and JavaScript. I'm also a lifelong learner who embraces new challenges!",
 };
 
 export default function RootLayout({
