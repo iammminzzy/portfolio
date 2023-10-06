28d6825326eda68580a24a7fff1a50b9bcc9f74a
minjee <minjeeson.dev@gmail.com>
2023-10-06 22:27:43 +0100

    lint: Add ESLint and Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+
 import Header from "../components/header/Header";
 
 export default function Home() {
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,8 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 import { links } from "@/libs/data";
+
 import NavItem from "./NavItem";
 
 export default function Header() {
