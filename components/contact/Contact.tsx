b1dd5de9b7e825ef6f60590155e8d7037468548b
minjee <minjeeson.dev@gmail.com>
2023-10-18 18:44:17 +0900

    feat: Added Contact section

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -1,4 +1,13 @@
+import { SectionTitle } from "@/components/common";
+
 const Contact = () => {
-  return <section>Contact</section>;
+  return (
+    <section
+      id="contact"
+      className="flex flex-col justify-start px-[10%] mx-auto w-full max-w-contentContainer min-h-screen py-24"
+    >
+      <SectionTitle title="Contact" />
+    </section>
+  );
 };
 export default Contact;
