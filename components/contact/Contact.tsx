88984eeb22e03d2a59f67b7397af1ec0c80387a2
minjee <minjeeson.dev@gmail.com>
2023-10-20 11:16:59 +0900

    chore: Contact section WIP

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -7,6 +7,12 @@ const Contact = () => {
       className="flex flex-col justify-start px-[10%] mx-auto w-full max-w-contentContainer min-h-screen py-24"
     >
       <SectionTitle title="Contact" />
+      <div className="flex flex-col w-full items-center  font-medium">
+        <p className="text-lg text-center">
+          Looking to team up on a project? Let&#39;s talk!
+          <br /> Drop me a line:)
+        </p>
+      </div>
     </section>
   );
 };
