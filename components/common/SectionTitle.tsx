0b25f61c71414a0287a915962d1ed03aedef4fdd
minjee <minjeeson.dev@gmail.com>
2023-10-25 17:00:00 +0900

    chore: modify the layout of contact section

--- components/common/SectionTitle.tsx
+++ components/common/SectionTitle.tsx
@@ -8,7 +8,7 @@ const SectionTitle = ({ titleNo, title }: SectionTitleProps) => {
     <h2 className="font-titleFont text-3xl font-medium flex items-center">
       <span className="text-base md:text-lg">{titleNo}</span>
       {title}
-      <span className="hidden md:inline-flex md:w-full lgl:2-72 h-0.5 bg-pointYellow ml-6" />
+      <span className="hidden md:inline-flex md:w-full h-0.5 bg-pointYellow ml-6" />
     </h2>
   );
 };
--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -4,14 +4,20 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="flex flex-col justify-start px-[10%] mx-auto w-full max-w-contentContainer min-h-screen py-24"
+      className="flex flex-col justify-start px-[10%] mx-auto w-full min-h-screen py-24"
     >
       <SectionTitle title="Contact" />
-      <div className="flex flex-col w-full items-center  font-medium">
-        <p className="text-lg text-center">
-          Looking to team up on a project? Let&#39;s talk!
-          <br /> Drop me a line:)
-        </p>
+      <div className="flex flex-col items-center pt-10">
+        {/* <h2 className="flex items-center gap-3 text-base mb-7">
+          <span className="hidden md:inline-flex md:w-20 h-0.5 bg-pointYellow" />
+          Contact
+          <span className="hidden md:inline-flex md:w-20 h-0.5 bg-pointYellow" />
+        </h2> */}
+        <p className="font-medium">Looking to team up on a project?</p>
+        <h3 className="mt-1 font-titleFont font-medium text-3xl">
+          Drop me a line !
+        </h3>
+        <div className="py-10">~email~</div>
       </div>
     </section>
   );
