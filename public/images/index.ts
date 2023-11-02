c650708ea7f0861ab12404e5683e591d0562d232
minjee <minjeeson.dev@gmail.com>
2023-11-02 19:41:13 +0000

    fix: fix error for build - renaming issue

--- public/images/index.ts
+++ public/images/index.ts
@@ -3,6 +3,5 @@ export { default as profileImg } from "./profile.png";
 export { default as ProjectImg_Admin } from "./projects/admin.png";
 export { default as ProjectImg_Beople } from "./projects/beople.jpg";
 export { default as ProjectImg_Knewnew } from "./projects/knewnew.gif";
-export { default as ProjectImg_Notiee } from "./projects/notiee.png";
 export { default as ProjectImg_Sesac } from "./projects/sesac.gif";
 export { default as ProjectImg_Turtlehome } from "./projects/turtlehome.gif";
