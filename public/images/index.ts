849540420e8d33bd580ca51e312ca85bc3d93d10
minjee <minjeeson.dev@gmail.com>
2023-11-02 19:37:30 +0000

    fix: fix error for build

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,8 +1,8 @@
 export { default as highlightIcon } from "./highlight.svg";
 export { default as profileImg } from "./profile.png";
-export { default as ProjectImg_Admin } from "./projects/Admin.png";
+export { default as ProjectImg_Admin } from "./projects/admin.png";
 export { default as ProjectImg_Beople } from "./projects/beople.jpg";
 export { default as ProjectImg_Knewnew } from "./projects/knewnew.gif";
-export { default as ProjectImg_Notiee } from "./projects/Notiee.png";
+export { default as ProjectImg_Notiee } from "./projects/notiee.png";
 export { default as ProjectImg_Sesac } from "./projects/sesac.gif";
 export { default as ProjectImg_Turtlehome } from "./projects/turtlehome.gif";
