65c07432ca003a5c235e26febed2c16c548c6d21
Minjee Son <104430030+iammminzzy@users.noreply.github.com>
2025-09-16 13:00:27 +0100

    Develop > Main (#28)
    * 1 (#26)
    
    * squash commit 1
    
    * squash commit 2
    
    * squash commit 3 (#27)

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Contact, Experience, Main, Projects } from "@/components";
 export default function Home() {
   return (
     <main className="w-full font-bodyFont overflow-x-hidden flex flex-col items-center px-[25px] sml:px-[50px] mdl:px-[100px] lgl:px-[150px]">
-      1
       <Main />
       <Experience />
       <Projects />
