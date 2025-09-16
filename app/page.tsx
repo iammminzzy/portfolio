36d0ba042b48d3ec15c8b2bc479b6cef83e28be2
Minjee Son <104430030+iammminzzy@users.noreply.github.com>
2025-09-16 11:58:10 +0100

    test commit 3 (#15)

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="w-full font-bodyFont overflow-x-hidden flex flex-col items-center px-[25px] sml:px-[50px] mdl:px-[100px] lgl:px-[150px]">
       <Main />
-      2
       <Experience />
       <Projects />
       <Contact />
